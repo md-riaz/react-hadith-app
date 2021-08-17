7b0ee66a9bd01827ca6d446b5466be8a6d9d3de7
md-riaz <riazmd582@gmail.com>
2021-08-17 20:55:05 +0600

    1 file modified

--- src/Hadith.jsx
+++ src/Hadith.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import parse from 'html-react-parser';
 
 export default function Hadith({ hadith, setCurrentComp }) {
